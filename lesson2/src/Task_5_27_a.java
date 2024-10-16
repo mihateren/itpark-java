722facc8637903f238cfd558f68e07d7ceba0b10
mihateren <mihateren2003@gmail.com>
2024-10-17 00:52:32 +0300

    add task_5_27_a

---
+++ lesson2/src/Task_5_27_a.java
@@ -0,0 +1,9 @@
+public class Task_5_27_a {
+    public static void main(String[] args) {
+        int ans = 0;
+        for (int i = 100; i <= 500; i++) {
+            ans += i;
+        }
+        System.out.println(ans);
+    }
+}
